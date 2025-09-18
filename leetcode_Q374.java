cd92740c9d8ae08c782250cc8681e07b7ae47d5d
Ankit Joshi <ankitjoshi9315@gmail.com>
2025-09-18 15:58:57 +0530

    leetcode solution

---
+++ leetcode_Q374.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class leetcode_Q374 {
+    public static void main(String[] args) {
+        int n = 10;
+        int x = 6;
+        System.out.println();
+        System.out.println(guessNumber(n, x));  // we will use API for this
+    }
+    static int guessNumber(int n,int x) {
+        int start = 1;
+        int end = n;
+        while (start<=end) {
+            int mid = start + (end - start) / 2;
+            if(mid == x){
+                return mid;
+            }else if(mid < x){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
