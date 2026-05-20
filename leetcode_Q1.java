099337365ea721667e871e194695588c5d94b96b
Ankit Joshi <ankitjoshi9315@gmail.com>
2026-05-20 21:04:36 +0530

    updated

--- leetcode_Q1.java
+++ leetcode_Q1.java
@@ -3,21 +3,54 @@
 package LeetCode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 
 public class Leetcode_Q1 {
     public static void main(String[] args) {
         System.out.println();
-        int[] arr = {3,2,4};
-        int target = 6;
+        int[] arr = {2,7,11,15};
+        int target = 9;
         int ans[] = find(arr,target);
         System.out.println(Arrays.toString(ans));
     }
     static int[] find(int[] nums,int target){
+
+        // method 1 : for indices
+
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=0;j<nums.length;j++){
+        //         if (nums[i]+nums[j] == target) {
+        //             return new int[] {i,j};
+        //         }
+        //     }
+        // }
+        // return new int[] {-1,-1};
+
+        // method 2: for value
+
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     int currAns = target - nums[i];
+        //     if(set.contains(currAns)){
+        //         return new int[] {nums[i],currAns};
+        //     }
+        //     else{
+        //         set.add(nums[i]);
+        //     }
+        // }
+        // return new int[] {-1,-1};
+
+        // method 3 : for indices using HashMap
+
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if (nums[i]+nums[j] == target) {
-                    return new int[] {i,j};
-                }
+            int currans = target - nums[i];
+            if(map.containsKey(currans)){
+                return new int[] {map.get(currans), i};
+            }else{
+                map.put(nums[i], i);
             }
         }
         return new int[] {-1,-1};
--- leetcode_Q1309.java
+++ leetcode_Q1309.java
@@ -7,8 +7,9 @@ public class leetcode_Q1309 {
     }
      static String freqAlphabets(String s) {
         StringBuilder sb = new StringBuilder();
+        // start loop from behind for checking if it is contains # or not
         for(int i=s.length()-1;i>=0;i--){
-            int number;
+            int number; 
             if(s.charAt(i) == '#'){
                 number = (s.charAt(i-2) - '0')*10 + (s.charAt(i-1) - '0');
                 i -= 2;
